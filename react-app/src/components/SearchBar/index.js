32336be588ae23ffa011f2eb75cda5a22df3b230
Greg Gardini <ggard214@gmail.com>
2021-02-25 16:08:44 -0700

    starting calendar search work

--- react-app/src/components/SearchBar/index.js
+++ react-app/src/components/SearchBar/index.js
@@ -1,31 +1,45 @@
-import "./SearchBar.css"
-import React from "react"
-import search_button from "../../img/airgng-search-button.png"
+import "./SearchBar.css";
+import React, { useState } from "react";
+import search_button from "../../img/airgng-search-button.png";
+import CalendarApp from "./calendar";
 
 const Search = () => {
-    return (
-        <div className="search">
-          <div className="search-location">
-            <div><b>Location</b></div>
-            <div>Where are you going?</div>
-          </div>
-          <div>
-            <div><b>Check In</b></div>
-            <div>Add Dates</div>
-          </div>
-          <div>
-            <div>Aircraft Carrier</div>
-            <div>What are you flying?</div>
-          </div>
-          <div>
-            <div>Guests</div>
-            <div>Add dates</div>
-          </div>
-          <button>
-              <img className="search-button" src={search_button}/>
-          </button>
-        </div>
-    );
+  const [showCal, setShowCal] = useState(false);
+
+  const openCal = () => {
+    if (showCal) return;
+    setShowCal(true);
   };
 
-  export default Search;
+  return (
+    <>
+      <div className="search">
+        <div className="search-location">
+          <div>
+            <b>Location</b>
+          </div>
+          <div>Where are you going?</div>
+        </div>
+        <div>
+          <button onClick={openCal}>
+            <b>Check In</b>
+          </button>
+          <div>Add Dates</div>
+        </div>
+        <div>
+          <div>Aircraft Carrier</div>
+          <div>What are you flying?</div>
+        </div>
+        <div>
+          <div>Guests</div>
+          <div>Add dates</div>
+        </div>
+        <button>
+          <img className="search-button" src={search_button} />
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Search;
