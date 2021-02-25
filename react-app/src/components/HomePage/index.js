96b0453c3dfb37c0b56189df68c0589e52b9cb40
kristenflorey <64163075+kristenflorey@users.noreply.github.com>
2021-02-25 16:51:19 -0500

    added search bar component

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -2,11 +2,12 @@ import "./Home.css";
 import React from "react";
 import sunrise_balloon from "../../img/sunrise-balloon.png"
 import striped_balloon from "../../img/striped-balloon.png"
-
+import Search from "../SearchBar"
 
 const Home = () => {
   return (
     <div >
+      <Search />
       <img className="striped-balloon" src={striped_balloon} />
       <img className="sunrise-balloon" src={sunrise_balloon}/>
     </div>
