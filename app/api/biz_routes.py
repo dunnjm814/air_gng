39b9cb63309af91c557762555726c7cdcb8e7458
Olga Thompson <olgatj2007@gmail.com>
2021-02-26 11:23:20 -0500

    user joined with review

--- app/api/biz_routes.py
+++ app/api/biz_routes.py
@@ -18,12 +18,26 @@ def one_biz(craft_id):
     else:
         return {}
 
+def to_service(review):
+    join_review = {
+      'id': review.id,
+      'rate': review.rate,
+      'title': review.title,
+      'comment': review.comment,
+      'user_id': review.user_id,
+      'service_id': review.service_id,
+      'username': review.user.username,
+      'email': review.user.email,
+      'profile_pic': review.user.profile_pic,
+    }
+    return join_review
+
 @biz_routes.route('/reviews/<int:craft_id>')
 def get_reviews(craft_id):
     reviews = Review.query.filter_by(service_id=craft_id).join(User).all()
     print ('ljdsflkajdlfkjsejrwjfsjdfjdsjkfd')
     # return {review.id: review.to_service(comment=review.comment, user=review.user) for review in reviews}
     if reviews:
-        return {review.id: review.to_dict() for review in reviews}
+        return {review.id: to_service(review) for review in reviews}
     else:
         return {}
