bbb0516594b7eaddea5a6f8b360a23baa17e065e
Jason Dunn <dunnjm814@gmail.com>
2021-02-28 19:28:12 -0700

    fixed panning! yay!

--- react-app/src/components/SearchBar/index.js
+++ react-app/src/components/SearchBar/index.js
@@ -1,6 +1,6 @@
 import "./SearchBar.css";
 import React, { useState, useEffect } from "react";
-import { Redirect, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import search_button from "../../img/airgng-search-button.png";
 import { enGB } from "date-fns/locale";
