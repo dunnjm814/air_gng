659f537b51a5dd39c030f04763edd18aa9592433
Greg Gardini <ggard214@gmail.com>
2021-02-28 21:55:39 -0700

    finalized css

--- react-app/src/components/UserProfile/AboutUserForm.js
+++ react-app/src/components/UserProfile/AboutUserForm.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import { useParams, useHistory } from 'react-router-dom'
 import { useDispatch } from "react-redux";
 import {submitProfile} from '../../store/profile'
+import './proform.css';
 
 
 
@@ -50,62 +51,75 @@ function AboutUserForm({ userProfile, info, setInfo }) {
   ]
   return (
     <>
+      <h1>hey its a form</h1>
       <form id="about-user-form-wrap" onSubmit={onSubmit}>
-        <h1>hey its a form</h1>
-        <label>about
+        <label>
+          about
           <input
             type="text"
             value={about}
             onChange={(e) => setAbout(e.target.value)}
           ></input>
         </label>
-        <label>first name
+        <label>
+          first name
           <input
             type="text"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
           ></input>
         </label>
-        <label>last name
+        <label>
+          last name
           <input
             type="text"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
           ></input>
         </label>
-        <label>phone number
+        <label>
+          phone number
           <input
             type="text"
             value={phoneNumber}
             onChange={(e) => setPhoneNumber(e.target.value)}
           ></input>
         </label>
-        <label>location
+        <label>
+          location
           <input
             type="text"
             value={location}
             onChange={(e) => setLocation(e.target.value)}
           ></input>
         </label>
-        <label>work
+        <label>
+          work
           <input
             type="text"
             value={work}
             onChange={(e) => setWork(e.target.value)}
           ></input>
         </label>
-        <label>select language
+        <label>
+          select language
           <select
             value={language}
             onChange={(e) => setLanguage(e.target.value)}
           >
             {languages.map((language) => {
-              return <option key={language.value} value={language.value}>{language.label}</option>
+              return (
+                <option key={language.value} value={language.value}>
+                  {language.label}
+                </option>
+              );
             })}
           </select>
         </label>
         <div>
-          <button onClick={(() => history.push(`/users/profile/${userId}`))}>cancel</button>
+          <button onClick={() => history.push(`/users/profile/${userId}`)}>
+            cancel
+          </button>
           <button type="submit">submit</button>
         </div>
       </form>
